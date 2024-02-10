2c9d40045a9fd84e71c3961e9257ade62e955ced
zam95 <zam95@hotmail.com>
2024-02-10 10:54:36 +0100

    more experiments

--- src/MinimalPlayground/WeatherEndpoint.cs
+++ src/MinimalPlayground/WeatherEndpoint.cs
@@ -1,4 +1,7 @@
-﻿namespace MinimalPlayground;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.OpenApi.Any;
+
+namespace MinimalPlayground;
 
 public static class WeatherEndpoint
 {
@@ -22,7 +25,38 @@ public static class WeatherEndpoint
                 return forecast;
             })
             .WithName("GetWeatherForecast")
-            .WithOpenApi();
+            .WithOpenApi(opt =>
+            {
+                opt.Summary = "Just another test endpoint summary";
+                opt.Description = "The default weather endpoint description";
+                return opt;
+            });
+        
+        app.MapGet("/hello", ([Required]string name) => new Hello(name))
+            .WithName("SayHello")
+            .WithOpenApi(opt =>
+            {
+                opt.Summary = "Hello to the workd";
+                opt.Description = "The welcome service endpoint";
+                opt.Parameters[0].Example = new OpenApiString("Bob");
+                return opt;
+            });
+        
+        app.MapGet("/complexInput", ([Required]AbstractSetting setting) => new SomeResult { IsOk = true, Setting = setting })
+            .WithName("ProcessComplexBody")
+            .WithOpenApi(opt =>
+            {
+                opt.Summary = "Checking how complex body with inheritance is handled";
+                opt.Description = "A complex input and output example";
+                // opt.Parameters[0].Examples.Add("doubleSetting", new OpenApiObject(new DoubleSetting
+                // {
+                //     Id = new Guid("8AF6FAEE-6F52-438A-9146-B59D6F3E580F"),
+                //     Number = 99,
+                //     Name = "My example"
+                // }));
+                
+                return opt;
+            });
 
         app.Run();
     }
@@ -30,5 +64,31 @@ public static class WeatherEndpoint
     record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
     {
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+
+    record Hello(string Message);
+
+    public abstract class AbstractSetting
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+    
+    public abstract class DoubleSetting : AbstractSetting
+    {
+        public double Number { get; set; }
+    }
+    
+    public abstract class StringSetting : AbstractSetting
+    {
+        public double Comment { get; set; }
+    }
+
+    public class SomeResult
+    {
+        public bool IsOk { get; set; }
+
+        public AbstractSetting Setting { get; set; }
     }
 }
