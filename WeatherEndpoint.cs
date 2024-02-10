af172a25f3fcc4359095addcfbdf67a628075c6f
zam95 <zam95@hotmail.com>
2024-02-10 09:23:10 +0100

    updating csproj to build rest api client from swagger file

---
+++ WeatherEndpoint.cs
@@ -0,0 +1,34 @@
+﻿namespace MinimalPlayground;
+
+public static class WeatherEndpoint
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public static void MapWeatherEndpoint(this WebApplication app)
+    {
+        app.MapGet("/weatherforecast", () =>
+            {
+                var forecast =  Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        (
+                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            Random.Shared.Next(-20, 55),
+                            Summaries[Random.Shared.Next(Summaries.Length)]
+                        ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+        app.Run();
+    }
+
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
